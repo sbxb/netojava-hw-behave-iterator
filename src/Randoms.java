242b1d2683058a300a39401adc1f70372ce17bdd
Vadim Persidskii <vadim@example.com>
2024-11-25 18:10:30 +0300

    Add Randoms class implementation

---
+++ src/Randoms.java
@@ -0,0 +1,32 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected final Random random = new Random();
+    protected final int minBound;
+    protected final int maxBound;
+
+    public Randoms(int min, int max) {
+        minBound = min;
+        maxBound = max;
+    }
+
+    protected int nextInt() {
+        return random.nextInt(minBound, maxBound + 1); // both bounds included
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return nextInt();
+            }
+        };
+    }
+}
